8fcdd57f28d99f789b3a8cc23ec70d9631b0db13
yuhsiang237 <umworksite@gmail.com>
2024-06-15 22:46:49 +0800

    1.切共享頁面與Partial (切Demo頁，並使用Partial) #1

---
+++ WebMVCApplication/WebMVCApplication/Controllers/DemoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebMVCApplication.ViewModels;
+
+namespace WebMVCApplication.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult UserIntro()
+        {
+            var nameCardViewModel = new NameCardViewModel
+            {
+                Name = "王小名",
+                Email = "xd123@gmail.com",
+                Address = "高雄市XXX區"
+            };
+            ViewBag.NameCardData = nameCardViewModel;
+
+            return View();
+        }
+
+    }
+}
