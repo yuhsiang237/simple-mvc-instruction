7f3b473868c1d35679e195cc917bda9b95afe397
yuhsiang237 <umworksite@gmail.com>
2024-06-16 15:29:40 +0800

    5.C#表單驗證與JQuery簡易常見操作方法，如加入監聽、控制畫面元件 #5

---
+++ WebMVCApplication/WebMVCApplication/Models/Validators/UserData.cs
@@ -0,0 +1,22 @@
+﻿namespace WebMVCApplication.Models.Validators
+{
+    public class UserData
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+
+        public int? Gender { get; set; }
+
+        public int? Age { get; set; }
+
+        public List<string>? Interests { get; set; }
+
+        public UserDetail? UserDetail { get; set; }
+    }
+
+    public class UserDetail
+    {
+        public string? School { get; set; }
+    }
+}
